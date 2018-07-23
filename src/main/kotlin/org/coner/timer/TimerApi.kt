875f9e1af38b999d2a721c384c7a0f42c9d9cec7
Carlton Whitehead <cebesius86@gmail.com>
2018-07-23 19:58:41 -0400

    Stub out a maven/kotlin project

---
+++ src/main/kotlin/org/coner/timer/TimerApi.kt
@@ -0,0 +1,5 @@
+package org.coner.timer
+
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
---
+++ src/test/kotlin/org/coner/timer/HelloWorldTest.kt
@@ -0,0 +1,14 @@
+package org.coner.timer
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class HelloWorldTest {
+
+    @Test
+    fun helloWorld() {
+        main(emptyArray())
+
+        assertTrue(true)
+    }
+}
