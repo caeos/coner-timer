a2f9b679c742fa97fb70536aaabaaa3444260a9f
Carlton Whitehead <cebesius86@gmail.com>
2018-07-27 19:38:28 -0400

    CLI allows timer input from comm port

--- cli/src/main/kotlin/org/coner/timer/cli/TimerInput.kt
+++ cli/src/main/kotlin/org/coner/timer/cli/TimerInput.kt
@@ -9,6 +9,7 @@ import com.github.ajalt.clikt.parameters.types.file
 import org.coner.timer.Timer
 import org.coner.timer.input.mapper.JACTimerInputMapper
 import org.coner.timer.input.reader.InputStreamTimerInputReader
+import org.coner.timer.input.reader.PureJavaCommTimerInputReader
 import org.coner.timer.output.FileAppendingOutputWriter
 import org.coner.timer.output.PrintlnTimerOutputWriter
 import purejavacomm.CommPortIdentifier
@@ -30,7 +31,7 @@ class TimerFileInput : CliktCommand(name = "input") {
     override fun run() {
         val config = InputStreamTimerInputReader.Config(inputFile.inputStream())
         val reader = InputStreamTimerInputReader(config)
-        val mapper = when(mapper) {
+        val mapper = when (mapper) {
             "jacircuits" -> JACTimerInputMapper()
             else -> throw IllegalStateException()
         }
@@ -41,14 +42,7 @@ class TimerFileInput : CliktCommand(name = "input") {
                 mapper = mapper,
                 mappedInputWriter = PrintlnTimerOutputWriter()
         )
-        TermUi.echo("Starting timer... ", trailingNewline = false)
-        timer.start()
-        TermUi.echo("OK")
-        TermUi.echo("Press Enter to stop...")
-        TermUi.confirm("", abort = false, promptSuffix = "", showDefault = false, default = true)
-        TermUi.echo("Stopping timer...", trailingNewline = false)
-        timer.stop()
-        TermUi.echo("OK")
+        runTimer(timer)
     }
 }
 
@@ -56,11 +50,38 @@ class TimerCommPort : CliktCommand(name = "comm-port") {
     override fun run() = Unit
 }
 
+class TimerCommPortInput : CliktCommand(name = "input") {
+    val portName: String by argument()
+            .choice(*CommPortIdentifier.getPortIdentifiers().toList().map { it.name }.toTypedArray())
+    val mapper: String by argument().choice("jacircuits")
+    val rawInputLogFile: File by argument().file(exists = true, folderOkay = false, writable = true)
+
+    override fun run() {
+        val config = PureJavaCommTimerInputReader.Config(
+                appName = "coner-timer-cli",
+                port = portName
+        )
+        val reader = PureJavaCommTimerInputReader(config)
+        val rawInputWriter = FileAppendingOutputWriter(rawInputLogFile)
+        val mapper = when (mapper) {
+            "jacircuits" -> JACTimerInputMapper()
+            else -> throw IllegalStateException()
+        }
+        val timer = Timer(
+                reader = reader,
+                rawInputWriter = rawInputWriter,
+                mapper = mapper,
+                mappedInputWriter = PrintlnTimerOutputWriter()
+        )
+        runTimer(timer)
+    }
+}
+
 class TimerCommPortList : CliktCommand(name = "list") {
     override fun run() {
         val ports = CommPortIdentifier.getPortIdentifiers().toList().sortedBy(CommPortIdentifier::getName)
         for (port in ports) {
-            TermUi.echo("Comm port: { name: ${port.name} }")
+            TermUi.echo("Comm port: { name: ${port.name}  }")
         }
     }
 }
@@ -70,6 +91,17 @@ fun main(args: Array<String>) = Timer()
                 TimerFile()
                         .subcommands(TimerFileInput()),
                 TimerCommPort()
-                        .subcommands(TimerCommPortList())
+                        .subcommands(TimerCommPortList(), TimerCommPortInput())
         )
         .main(args)
+
+inline fun <RTI, I> runTimer(timer: Timer<RTI, I>) {
+    TermUi.echo("Starting timer... ", trailingNewline = false)
+    timer.start()
+    TermUi.echo("OK")
+    TermUi.echo("Press Enter to stop...")
+    TermUi.confirm("", abort = false, promptSuffix = "", showDefault = false, default = true)
+    TermUi.echo("Stopping timer...", trailingNewline = false)
+    timer.stop()
+    TermUi.echo("OK")
+}
